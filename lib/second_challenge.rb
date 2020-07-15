e90af8b36a76b4c36bf8148b7747c426d56bce0e
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-15 17:26:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,9 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       patriarch: {}
-      matriarch {}
+      matriarch: {}
       hero: {}
-      hero_friends []
+      hero_friends: []
       }, 
     :capulet => {
       patriarch: {}
