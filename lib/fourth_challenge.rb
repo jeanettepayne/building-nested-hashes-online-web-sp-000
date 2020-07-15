a1798ca798ab7bd83517c4f7db11731d0358874c
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-15 17:40:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,12 +9,13 @@ def fourth_challenge
           :name => "Benvolio",
          :age => "17",
          :attitude => "worried"
-        }
+        },
         {
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
-        }]
+        }
+      ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -25,12 +26,13 @@ def fourth_challenge
           :name => "Steven",
           :age => "30",
           :attitude => "confused"
-        }
+        },
         {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
-        }]
+        }
+      ]
    }
 }
 
